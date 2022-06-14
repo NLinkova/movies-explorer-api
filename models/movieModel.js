732ba020792e626bf441f201b0aa9f3a54f6bd4c
Natalia Linkova <linkova1109@yandex.ru>
2022-06-14 20:48:26 +1000

    schemas done

---
+++ models/movieModel.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate(link) {
+      return /http[s]?:\/\/(www.)?[\S]+\.[a-z]+[\S]*/gi.test(link);
+    },
+  },
+  trailerLink: {
+    type: String,
+    required: true,
+    validate(link) {
+      return /http[s]?:\/\/(www.)?[\S]+\.[a-z]+[\S]*/gi.test(link);
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate(link) {
+      return /http[s]?:\/\/(www.)?[\S]+\.[a-z]+[\S]*/gi.test(link);
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User',
+  },
+  movieId: {
+    type: String,
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('movie', movieSchema);
