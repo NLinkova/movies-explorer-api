768f9d55c7ee19ae7534985d06c2acc9a43a49f9
Natalia Linkova <linkova1109@yandex.ru>
2022-06-20 12:35:24 +1000

    cors

--- helpers/cors-options.js
+++ helpers/cors-options.js
@@ -1,9 +1,9 @@
 const CORS_CONFIG = {
   origin: [
-    'https://linkova.mesto.back.nomoredomains.xyz',
-    'http://linkova.mesto.back.nomoredomains.xyz',
-    'https://linkova.mesto.front.nomoredomains.xyz',
-    'http://linkova.mesto.front.nomoredomains.xyz',
+    'https://linkova.movie.back.nomoredomains.xyz',
+    'http://linkova.movie.back.nomoredomains.xyz',
+    'https://linkova.movie.front.nomoredomains.xyz',
+    'http://linkova.movie.front.nomoredomains.xyz',
     'http://localhost:3000',
   ],
 };
